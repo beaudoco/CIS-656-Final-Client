fbb89dcbb4500968321f223eee1d5a45aa463793
beaudoco <beaudoco@mail.gvsu.edu>
2020-11-10 23:54:37 -0500

    updating port

--- ResponseRemoteImpl.java
+++ ResponseRemoteImpl.java
@@ -25,7 +25,7 @@ public class ResponseRemoteImpl implements Response {
 
             if (response.toString().contains("/")) {
                 String hostIP = response.toString().split(":")[0];
-                Socket sock2 = new Socket(hostIP, 8080);
+                Socket sock2 = new Socket(hostIP, 22);
 
                 System.out.println("big ben");
             }
