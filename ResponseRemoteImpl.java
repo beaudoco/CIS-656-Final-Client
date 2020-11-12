9133f669623a7bb47aa23aee6122e84b5d7f4c80
beaudoco <beaudoco@mail.gvsu.edu>
2020-11-11 22:48:50 -0500

    checking result from server

--- ResponseRemoteImpl.java
+++ ResponseRemoteImpl.java
@@ -161,5 +161,48 @@ class ServerThread extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        boolean hasValue = true;
+
+        while(hasValue) {
+            String request = null;
+            try {
+                ObjectInputStream isr = new ObjectInputStream(sock.getInputStream());
+                Object object = isr.readObject();
+
+                if (object instanceof  StringRpcRequest) {
+                    StringRpcRequest stringRpcRequest = (StringRpcRequest) object;
+
+                    String tmpString = stringRpcRequest.getString();
+
+                    if ("request".equals(stringRpcRequest.getMethod())) {
+//                        if (tmpString.toLowerCase().equals("time")) {
+//                            request = response.timeString();
+//                        } else if (tmpString.isEmpty()) {
+////                            System.out.println(clientList.getClients().isEmpty());
+//                            clientList.removeClient(clientName);
+//                            hasValue = false;
+//                            sock.close();
+//                            System.out.println("Socket closed!");
+//                        } else {
+//                            request = response.capitalizeString(tmpString);
+//                        }
+                        request = tmpString;
+
+                        if (hasValue) {
+                            out = new ObjectOutputStream(sock.getOutputStream());
+                            out.writeObject(request);
+                            out.flush();
+                        }
+                    }
+
+                } else {
+                    System.out.println("error!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("error!");
+            }
+        }
     }
 }
