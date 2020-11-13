b2d0032780fb01d5636dadd4bd26f0d80f4795dc
beaudoco <beaudoco@mail.gvsu.edu>
2020-11-13 18:16:16 -0500

    adding host

--- ResponseRemoteImpl.java
+++ ResponseRemoteImpl.java
@@ -50,7 +50,7 @@ public class ResponseRemoteImpl implements Response {
                     }
                     hostIP = response.toString();
                 }
-                System.out.println("host: " + hostIP);
+                clientList.addClient(hostIP);
                 System.out.println(response);
 
                 while (clientHasValue) {
